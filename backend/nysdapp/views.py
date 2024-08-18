f3312d05dc9b4346412d477d5a32bc8f78281ea6
NYSD DEREK <124192421+nysd01@users.noreply.github.com>
2024-08-18 20:37:02 +0100

    RE ARRANGEMENT

--- backend/nysdapp/views.py
+++ backend/nysdapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib.auth.models import User
+from django.contrib import messages
+
 
 def home(request):
     return render(request, 'main.html')
@@ -10,10 +13,38 @@ def chat(request):
     return render(request, 'chat.html')
 
 def login(request):
-    return render(request, 'Login.html')
+    return render(request, 'login.html')
 
 def service(request):
     return render(request, 'services.html')  
 
 def register(request):
-    return render(request, 'Register.html')       
+    if request.method == "POST":
+        username = request.POST.get('username')
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        email = request.POST.get('email')
+        pass1 = request.POST.get('pass1')
+        pass2 = request.POST.get('pass2')
+
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = firstname
+        myuser.last_name = lastname
+
+        myuser.save()
+
+        messages.success(request, 'your account has been successfully created.')
+        return redirect('login')
+
+    return render(request, 'register.html')
+
+def search_view(request):
+    if request.method == 'GET':
+        search_term = request.GET.get('q')
+        if search_term:
+            # You can add your search logic here
+            # For example, you can use Django's ORM to search a model
+            # results = MyModel.objects.filter(name__icontains=search_term)
+            results = ['arduino uno, sensors, leds, motors']  # Replace with your search results
+            return render(request, 'search_results.html', {'results': results})
+    return redirect('home')           
