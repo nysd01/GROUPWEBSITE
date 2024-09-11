e38961fa15f871ec4754d77b25d3cdeb0d4aef60
typw1 <pierretypw@gmail.com>
2024-09-11 15:00:23 +0200

    triedoutsome vercelstuffs

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,2 +1,5 @@
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+#python3.9 manage.py collectstatic
+#!/bin/bash
+python3 manage.py collectstatic --noinput
+python3 manage.py migrate
