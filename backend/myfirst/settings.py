2aac85014aaf4e3e0262a2f98f555dc7b9d7cbab
TYPW1 <pierretypw@gmail.com>
2024-09-11 20:27:26 +0200

    Add Whitenoise for static files

--- backend/myfirst/settings.py
+++ backend/myfirst/settings.py
@@ -142,6 +142,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 if DEBUG:
     INSTALLED_APPS += ['debug_toolbar']
     MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
