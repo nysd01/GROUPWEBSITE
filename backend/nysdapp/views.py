4071c27e2d503c404eba1540df46c2800a977016
TYPW01 <pierretypw@gmail.com>
2024-09-12 11:05:45 +0200

    trying out database

--- backend/nysdapp/views.py
+++ backend/nysdapp/views.py
@@ -125,17 +125,6 @@ def subscribe(request):
     
     return render(request, 'main.html', {'form': form})
 
-# Middleware to ensure session exists
-class EnsureSessionMiddleware:
-    def __init__(self, get_response):
-        self.get_response = get_response
-
-    def __call__(self, request):
-        if not request.session.session_key:
-            request.session.create()
-        response = self.get_response(request)
-        return response
-
 
 def add_to_cart(request, product_id):
     product = get_object_or_404(Product, id=product_id)
