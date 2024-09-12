343e79fb13b57750b5e96ee7774c48a2395a14aa
TYPW01 <pierretypw@gmail.com>
2024-09-12 10:54:55 +0200

    trying out database

--- backend/myfirst/settings.py
+++ backend/myfirst/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
-    'myapp.middleware.EnsureSessionMiddleware',
+    'nysdapp.middleware.EnsureSessionMiddleware',
        
 ]
 
