81fc9425ee70c84abea5ff42068e65f00e3d05fd
NYSD DEREK <124192421+nysd01@users.noreply.github.com>
2024-09-12 14:10:40 +0100

    keep connection

--- backend/myfirst/settings.py
+++ backend/myfirst/settings.py
@@ -96,6 +96,7 @@ WSGI_APPLICATION = 'myfirst.wsgi.application'
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'CONN_MAX_AGE': 60,
     }
 }"""
 
